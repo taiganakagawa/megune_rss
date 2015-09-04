143aa65e199ce6080f771046cee2d916226bd952
中川大雅 <neidhardt0209@gmail.com>
2015-09-04 12:56:15 +0900

    アクセスするたびにめぐねえの表情が変わる

--- main.rb
+++ main.rb
@@ -4,7 +4,9 @@ require 'sinatra/reloader'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
+
 get '/' do
+	@megune = rand(3)
 	@rss = RSS::Parser.parse("http://b.hatena.ne.jp/hotentry.rss")
 	erb :index
 	
